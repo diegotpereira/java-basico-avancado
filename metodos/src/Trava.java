c14d19d1a79390d384abb8e367be62b303239144
diegotpereira <diegoteixeirapereira@hotmail.com>
2022-06-15 08:13:01 -0300

    criação parcial da classe trava.

---
+++ metodos/src/Trava.java
@@ -0,0 +1,20 @@
+public class Trava {
+    
+    public String quem;
+
+    public boolean travado;
+
+    public void trave(String q) {
+        this.travado = true;
+        this.quem = q;
+    }
+
+    public void destrave(String q) {
+        this.travado = false;
+        this.quem = q;
+    }
+
+    public boolean estado() {
+        return travado;
+    }
+}
