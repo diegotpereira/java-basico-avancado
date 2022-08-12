4874843459af026d53a9f44749c294742712bd3c
diegotpereira <diegoteixeirapereira@hotmail.com>
2022-08-12 11:32:44 -0300

    criação parcial do componente pagar conta.

--- exemplos-praticos/sistema-faturamento-eletricidade/src/PagarConta.java
+++ exemplos-praticos/sistema-faturamento-eletricidade/src/PagarConta.java
@@ -3,12 +3,29 @@ import java.awt.event.*;
 import javax.swing.*;
 import java.sql.*;
 
-public class PagarConta extends JFrame implements ActionListener{
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        // TODO Auto-generated method stub
-        
-    }
+public class PagarConta extends JFrame{
     
+    PagarConta() {
+
+        JEditorPane editorPane = new JEditorPane();
+        editorPane.setEditable(false);
+
+        try {
+            editorPane.setPage("https://www.rge-rs.com.br/");
+
+        } catch (Exception e) {
+            editorPane.setContentType("text/html");
+            editorPane.setText("<html>Não foi possível carregar</html>");
+        }
+        JScrollPane scrollPane = new JScrollPane(editorPane);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        getContentPane().add(scrollPane);
+        setPreferredSize(new Dimension(800, 600));
+        setSize(800, 800);
+        setLocation(250, 120);
+        setVisible(true);
+    }
+    public static void main(String[] args) {
+        new PagarConta().setVisible(true);
+    }
 }
