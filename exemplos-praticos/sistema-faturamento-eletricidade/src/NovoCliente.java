072ee4e05ed3eb5bdabe48092054b49281e21e4d
diegotpereira <diegoteixeirapereira@hotmail.com>
2022-08-12 09:03:47 -0300

    criação parcial das classes java JFrame.

---
+++ exemplos-praticos/sistema-faturamento-eletricidade/src/NovoCliente.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.sql.*;
+
+public class NovoCliente extends JFrame implements ActionListener{
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
