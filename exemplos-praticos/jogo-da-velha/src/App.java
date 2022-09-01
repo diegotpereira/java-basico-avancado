22f208a51462f83cf9a60bd7581aad7b41652c57
diegotpereira <diegoteixeirapereira@hotmail.com>
2022-09-01 16:01:03 -0300

    criação parcial do template.

--- exemplos-praticos/jogo-da-velha/src/App.java
+++ exemplos-praticos/jogo-da-velha/src/App.java
@@ -69,11 +69,13 @@ public class App extends JFrame{
         jPanel1.setLayout(jPainel1Layout);
 
         jPainel1Layout.setHorizontalGroup(
-            jPainel1Layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            jPainel1Layout.createParallelGroup(Alignment.LEADING)
             .addGroup(jPainel1Layout.createSequentialGroup()
             .addGap(309, 309, 309)
             .addComponent(jLabel1)
-            .addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jLabel4)
+            .addContainerGap())
         );
 
         jPainel1Layout.setVerticalGroup(
@@ -83,7 +85,8 @@ public class App extends JFrame{
             .addComponent(jLabel1)
             .addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
             .addGroup(GroupLayout.Alignment.TRAILING, jPainel1Layout.createSequentialGroup()
-            .addGap(0, 0, Short.MAX_VALUE))
+            .addGap(0, 0, Short.MAX_VALUE)
+            .addComponent(jLabel4))
         );
 
         GroupLayout jPainel2Layout = new GroupLayout(jPanel2);
@@ -127,7 +130,32 @@ public class App extends JFrame{
             .addGap(28, 28, 28)
             .addGroup(layout.createParallelGroup(Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-            )))
+            .addComponent(txtbtn1, GroupLayout.PREFERRED_SIZE, 150, GroupLayout.PREFERRED_SIZE)
+            .addPreferredGap(ComponentPlacement.UNRELATED)
+            .addComponent(txtbtn2, GroupLayout.PREFERRED_SIZE, 150, GroupLayout.PREFERRED_SIZE))
+            .addGroup(layout.createSequentialGroup()
+            .addComponent(txtbtn4, GroupLayout.PREFERRED_SIZE, 150, GroupLayout.PREFERRED_SIZE)
+            .addPreferredGap(ComponentPlacement.UNRELATED)
+            .addComponent(txtbtn5, GroupLayout.PREFERRED_SIZE, 150, GroupLayout.PREFERRED_SIZE))
+            .addGroup(layout.createSequentialGroup()
+            .addComponent(txtbtn7, GroupLayout.PREFERRED_SIZE, 150, GroupLayout.PREFERRED_SIZE)
+            .addPreferredGap(ComponentPlacement.UNRELATED)
+            .addComponent(txtbtn8, GroupLayout.PREFERRED_SIZE, 150, GroupLayout.PREFERRED_SIZE)))
+            .addPreferredGap(ComponentPlacement.UNRELATED)
+            .addGroup(layout.createParallelGroup(Alignment.LEADING)
+            .addComponent(txtbtn3, GroupLayout.PREFERRED_SIZE, 150, GroupLayout.PREFERRED_SIZE)
+            .addComponent(txtbtn6, GroupLayout.PREFERRED_SIZE, 150, GroupLayout.PREFERRED_SIZE)
+            .addComponent(txtbtn9, GroupLayout.PREFERRED_SIZE, 150, GroupLayout.PREFERRED_SIZE))
+            .addGroup(layout.createParallelGroup(Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+            .addGap(28, 28, 28)
+            .addComponent(jButton10, GroupLayout.PREFERRED_SIZE, 235, GroupLayout.PREFERRED_SIZE)
+            .addGap(35, 35, 35)
+            .addComponent(jButton11, GroupLayout.PREFERRED_SIZE, 235, GroupLayout.PREFERRED_SIZE))
+            .addGroup(layout.createSequentialGroup()
+            .addGap(90, 90, 90)
+            .addComponent(jPanel2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
+            .addContainerGap(41, Short.MAX_VALUE))
         );
 
         layout.setVerticalGroup(
@@ -136,7 +164,34 @@ public class App extends JFrame{
             .addComponent(jPanel1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
             .addGap(24, 24, 24)
             .addGroup(layout.createParallelGroup(Alignment.TRAILING, false)
-            .addGroup(layout.createSequentialGroup())))
+            .addGroup(layout.createSequentialGroup()
+            .addComponent(txtbtn3, GroupLayout.PREFERRED_SIZE, 150, GroupLayout.PREFERRED_SIZE)
+            .addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(txtbtn6, GroupLayout.PREFERRED_SIZE, 150, GroupLayout.PREFERRED_SIZE))
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+            .addGroup(layout.createParallelGroup(Alignment.BASELINE)
+            .addComponent(txtbtn1, GroupLayout.PREFERRED_SIZE, 150, GroupLayout.PREFERRED_SIZE)
+            .addComponent(txtbtn2, GroupLayout.PREFERRED_SIZE, 150, GroupLayout.PREFERRED_SIZE))
+            .addPreferredGap(ComponentPlacement.RELATED)
+            .addGroup(layout.createParallelGroup(Alignment.BASELINE)
+            .addComponent(txtbtn4, GroupLayout.PREFERRED_SIZE, 150, GroupLayout.PREFERRED_SIZE)
+            .addComponent(txtbtn5, GroupLayout.PREFERRED_SIZE, 150, GroupLayout.PREFERRED_SIZE)))
+            .addComponent(jPanel2, GroupLayout.Alignment.TRAILING, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
+            .addGroup(layout.createParallelGroup(Alignment.LEADING)
+            .addGroup(GroupLayout.Alignment.TRAILING, layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+            .addGap(19, 19, 19)
+            .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+            .addComponent(jButton10, GroupLayout.PREFERRED_SIZE, 92, GroupLayout.PREFERRED_SIZE)
+            .addComponent(jButton11, GroupLayout.PREFERRED_SIZE, 92, GroupLayout.PREFERRED_SIZE)))
+            .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+            .addComponent(txtbtn7, GroupLayout.PREFERRED_SIZE, 150, GroupLayout.PREFERRED_SIZE)
+            .addComponent(txtbtn8, GroupLayout.PREFERRED_SIZE, 150, GroupLayout.PREFERRED_SIZE)))
+            .addGroup(GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+            .addPreferredGap(ComponentPlacement.RELATED)
+            .addComponent(txtbtn9, GroupLayout.PREFERRED_SIZE, 150, GroupLayout.PREFERRED_SIZE)))
+            .addGap(0, 23, Short.MAX_VALUE))
         );
         pack();
         setLocationRelativeTo(null);
