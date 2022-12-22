975e9e1daa7f3f780a07032cb1f0263b45dfea60
diegotpereira <diegoteixeirapereira@hotmail.com>
2022-12-22 09:13:38 -0300

    salvar alterações.

--- exemplos-praticos/java-questoes-hackerrank/java-visitor-pattern/src/App.java
+++ exemplos-praticos/java-questoes-hackerrank/java-visitor-pattern/src/App.java
@@ -1,5 +1,5 @@
 import java.util.*;
-<<<<<<< HEAD
+
 
 enum Color {
     RED, GREEN
@@ -241,55 +241,6 @@ public class App {
       	System.out.println(res1);
      	System.out.println(res2);
     	System.out.println(res3);
-=======
-import java.awt.Color;
 
-public class App {
-    public static void main(String[] args) throws Exception {
-
-        Scanner teclado = new Scanner(System.in);
-
-        int n = teclado.nextInt();
-
-        Map<Integer, Object[]> nodeAtts = new HashMap<Integer, Object[]>();
-
-        for(int i = 0; i < n; i++) 
-
-            nodeAtts.put(i + 1, new Object[] {
-
-                teclado.nextInt(), null
-            });
-        
-
-        for(int i = 0; i < n; i++) 
-
-            nodeAtts.get(i + 1)[1] = teclado.nextInt() == 0 ? Color.RED : Color.GREEN;
-
-            System.out.println(Color.RED);
-
-        Map<Integer, ArrayList<Integer>> arestas = new HashMap<Integer, ArrayList<Integer>>();
-
-        for(int i = 1; i < n; i++) 
-
-            arestas.put(i, new ArrayList<Integer>());
-
-        
-
-        for(int i = 1; i < n; i++) {
-
-            int u = teclado.nextInt();
-            int v = teclado.nextInt();
-
-            arestas.get(u).add(v);
-            arestas.get(v).add(u);
-
-        }
-
-
-
-        
-        teclado.close();
-        System.out.println("Hello, World!");
->>>>>>> 5769b78b35bf198305092247cf70ba77d745204b
     }
 }
