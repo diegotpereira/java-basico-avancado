d58441b77a756f32fc8ed4a1ffbe4ce80610f957
diegotpereira <diegoteixeirapereira@hotmail.com>
2023-03-21 11:56:24 -0300

    criação da solução java

--- exemplos-praticos/java-questoes-hackerrank/Algorithms/implementation/strange-counter/src/Resultado.java
+++ exemplos-praticos/java-questoes-hackerrank/Algorithms/implementation/strange-counter/src/Resultado.java
@@ -3,28 +3,43 @@ public class Resultado {
 
     public static long strangeCounter(long t) {
 
-        long x = 0L;
-        long i = 0L;
-        long s = 0L;
-        long k = 1L;
-
-        while(x < t) {
-
-            // System.out.println(Math.pow(2, i));
-
-            x = (long) (3 * Math.pow(2, i));
-            k += x;
-
-            if (k > t) {
-                
-                s = k - t;
-                break;
-            }
-
-            i++;
+        long tamanhoMaximo = 3;
+        long contar = 0;
+        
+        while(contar < t) {
+            
+            contar += tamanhoMaximo;
+            tamanhoMaximo *= 2;
         }
-
-        return s;
+        
+        return (tamanhoMaximo / 2) - ((tamanhoMaximo / 2) - ((contar - t) + 1));
+    
     }
+
+    // public static long strangeCounter(long t) {
+
+    //     long x = 0L;
+    //     long i = 0L;
+    //     long s = 0L;
+    //     long k = 1L;
+
+    //     while(x < t) {
+
+    //         // System.out.println(Math.pow(2, i));
+
+    //         x = (long) (3 * Math.pow(2, i));
+    //         k += x;
+
+    //         if (k > t) {
+                
+    //             s = k - t;
+    //             break;
+    //         }
+
+    //         i++;
+    //     }
+
+    //     return s;
+    // }
 
 }
