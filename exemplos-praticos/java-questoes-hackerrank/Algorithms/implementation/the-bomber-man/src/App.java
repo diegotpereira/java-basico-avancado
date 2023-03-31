a3bd686af99cd0918122c86cf71a2e5490946451
diegotpereira <diegoteixeirapereira@hotmail.com>
2023-03-31 09:39:24 -0300

    alteração classe java

--- exemplos-praticos/java-questoes-hackerrank/Algorithms/implementation/the-bomber-man/src/App.java
+++ exemplos-praticos/java-questoes-hackerrank/Algorithms/implementation/the-bomber-man/src/App.java
@@ -39,8 +39,78 @@ public class App {
 
 class Resultado {
 
-    public static List<String> bomberMan(int n, List<String> grade) {
-        return null;
+    public static List<String> bomberMan(int n, List<String> grid) {
+        
+        char[][] newGrid = new char[grid.size()][grid.get(0).length()];
+        for (int i = 0; i < newGrid.length; i++) {
+            for (int j = 0; j < newGrid[0].length; j++) {
+                newGrid[i][j] = grid.get(i).charAt(j);
+            }
+        }
+        
+        char[][] firstState = getStates(newGrid, false);
+        char[][] secondState = getStates(firstState, false);
+        
+        boolean next = false;
+        int state = -1;
+        
+        for (int i = 2; i <= n; i++) {
+            if (state != 0) {
+                state = 0;
+                continue;
+            }
+            
+            if (next) state = 1;
+            else state = 2;
+            next = !next;
+        }
+        
+        if (state == -1) return grid;
+        if (state == 0) return convertBack(firstState, true);
+        if (next) return convertBack(firstState, false);
+        return convertBack(secondState, false);
     }
+    
+    private static List<String> convertBack(char[][] grid, boolean empty) {
+        List<String> res = new ArrayList<>();
+        for (int i = 0; i < grid.length; i++) {
+            StringBuilder cur = new StringBuilder();
+            for (int j = 0; j < grid[0].length; j++) {
+                if (empty) cur.append('O');
+                else cur.append(grid[i][j]);
+            }
+            
+            res.add(cur.toString());
+        }
+        
+        return res;
+    }
+    
+    private static char[][] getStates(char[][] grid, boolean fill) {
+        Set<Integer[]> bombs = new HashSet<>();
+        char[][] newGrid = new char[grid.length][grid[0].length];
+        
+        for (int i = 0; i < grid.length; i++) {
+            newGrid[i] = grid[i].clone();
+        }
+        
+        for (int i = 0; i < newGrid.length; i++) {
+            for (int j = 0; j < newGrid[i].length; j++) {
+                if (newGrid[i][j] == 'O') bombs.add(new Integer[] {i, j});
+                else newGrid[i][j] = 'O';
+            }
+        }
+        
+        for (Integer[] bomb : bombs) {
+            newGrid[bomb[0]][bomb[1]] = '.';
+            if (bomb[0] - 1 >= 0) newGrid[bomb[0] - 1][bomb[1]] = '.';
+            if (bomb[1] + 1 < newGrid[0].length) newGrid[bomb[0]][bomb[1] + 1] = '.';
+            if (bomb[0] + 1 < newGrid.length) newGrid[bomb[0] + 1][bomb[1]] = '.';
+            if (bomb[1] - 1 >= 0) newGrid[bomb[0]][bomb[1] - 1] = '.';
+        }
+        
+        return newGrid;
+    }
+
     
 }
