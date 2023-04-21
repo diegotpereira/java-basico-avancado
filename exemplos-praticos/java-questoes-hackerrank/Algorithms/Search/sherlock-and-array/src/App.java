65d48e2af5abdcdb0093b0ec556cf32e60f8f5f3
diegotpereira <diegoteixeirapereira@hotmail.com>
2023-04-21 11:02:47 -0300

    criação das soluções java

--- exemplos-praticos/java-questoes-hackerrank/Algorithms/Search/sherlock-and-array/src/App.java
+++ exemplos-praticos/java-questoes-hackerrank/Algorithms/Search/sherlock-and-array/src/App.java
@@ -1,5 +1,35 @@
+import java.io.*;
+import java.util.stream.*;
+import java.util.*;
+import static java.util.stream.Collectors.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, T).forEach(TItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                        .map(Integer::parseInt)
+                        .collect(toList());
+
+                String result = Resultado.balancedSums(arr);
+
+                bufferedWriter.write(result);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
         System.out.println("Hello, World!");
     }
 }
