1630f8e94b6a494fb95fce6d4adc04eac339895a
diegotpereira <diegoteixeirapereira@hotmail.com>
2023-04-26 16:57:20 -0300

    criação das soluções.

--- exemplos-praticos/java-questoes-hackerrank/Algorithms/Greedy/two-arrays/src/Resultado.java
+++ exemplos-praticos/java-questoes-hackerrank/Algorithms/Greedy/two-arrays/src/Resultado.java
@@ -1,7 +1,4 @@
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 // O problema "Two Arrays" do HackerRank é um desafio de programação que envolve a criação 
